acbb1dcc71d0352bfe5b52bf8721e858ac1872ce
marcus vogado <marcus_620@hotmail.com>
2023-04-23 01:22:33 -0300

    update

--- EntendendoAlgoritimos/Algoritimos/Pesquisa_Binaria.cs
+++ EntendendoAlgoritimos/Algoritimos/Pesquisa_Binaria.cs
@@ -29,5 +29,5 @@ namespace EntendendoAlgoritimos.Algoritimos
             }
             return resultado;
         }        
-    }
+    }   
 }
--- EntendendoAlgoritimos/Program.cs
+++ EntendendoAlgoritimos/Program.cs
@@ -2,12 +2,9 @@
 using EntendendoAlgoritimos.Algoritimos;
 
 public class Program
-{
-  
-
+{ 
     public static void Main(string[] args)
     {
-
                            //PESQUISA BINARIA
         
         List<int> minhaLista_Int = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,14 };        
