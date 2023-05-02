a03e6ac47ffa2bfb816d59aec787dec7a552e050
marcus vogado <marcus_620@hotmail.com>
2023-05-01 23:41:04 -0300

    update QuickSort pegando elemento central da tabela para dividir e torna a ordenação dos intems mais rápida.

--- EntendendoAlgoritimos/Algoritimos/QuickSort.cs
+++ EntendendoAlgoritimos/Algoritimos/QuickSort.cs
@@ -10,11 +10,12 @@ namespace EntendendoAlgoritimos.Algoritimos
     {
         public static IEnumerable<int>QuickSor(IEnumerable<int> list)
         {
-            if (list.Count() <= 1) return list;//verificando se a lista contem menos de 2 elementos
-            var pivot = list.First();// pegando o primeiro elemento como pivô
-            var menor = list.Skip(1).Where(i => i <= pivot); // ordenando os números menores que o pivô
-            var maior = list.Skip(1).Where(i => i > pivot);//ordenando os números maiores que o pivô
-            return QuickSor(menor).Union(new List<int> { pivot }).Union(QuickSor(maior));// unindo a lista de menor, pivô e maior.
+            if (list.Count() <= 1) return list;//verificando se a lista contem menos de 2 elementos            
+            var meio= list.Count() / 2;// pegando o elemento central como pivô
+            var pivo= list.FirstOrDefault(meio);//passando o elemento central por FirstiOrDefault para descobrir o elemento central da tabela.
+            var menor = list.Skip(1).Where(i => i <= pivo); // ordenando os números menores que o pivô
+            var maior = list.Skip(1).Where(i => i > pivo);//ordenando os números maiores que o pivô
+            return QuickSor(menor).Union(new List<int> { pivo }).Union(QuickSor(maior));// unindo a lista de menor, pivô e maior.
         }
     }
 }
--- EntendendoAlgoritimos/Program.cs
+++ EntendendoAlgoritimos/Program.cs
@@ -3,17 +3,17 @@ using EntendendoAlgoritimos.Algoritimos;
 using System.Text;
 
 public class Program
-{ 
+{
     public static void Main(string[] args)
     {
         #region PESQUISA BINARIA
         //Pesquisando por Número do índice
         Console.WriteLine("Pesquisando por Numero do índice");
-        List<int> minhaLista_Int = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,14 };        
+        List<int> minhaLista_Int = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
         Console.WriteLine(Pesquisa_Binaria.PesquisaBinariaInt(minhaLista_Int, 6));
         //Pesquisando por String, Email e etc.
         Console.WriteLine("Pesquisando por String, Email e etc.");
-        List<string> minhaListaString = new List<string>() {"Marcus", "Silmara", "Alberto", "Alfredo" };
+        List<string> minhaListaString = new List<string>() { "Marcus", "Silmara", "Alberto", "Alfredo" };
         Console.WriteLine("O Índice é :" + Pesquisa_Binaria.PesquisaBinariastring(minhaListaString, "Marcus"));
         Console.WriteLine("_____________________________");
         #endregion
@@ -21,7 +21,7 @@ public class Program
         #region ORDENAÇÃO POR SELEÇÃO
         Console.WriteLine("ORDENAÇÃO POR SELEÇÃO");
         var numbers = new int[] { 4, 5, 1, 3, 10, 9, 6, 8, 7, 2 };
-        var sortedArr =OrdenacaoPorSelecao.SelectionSort(numbers);
+        var sortedArr = OrdenacaoPorSelecao.SelectionSort(numbers);
         Console.WriteLine(string.Join(',', sortedArr));
         Console.WriteLine("_____________________");
         #endregion
@@ -30,7 +30,7 @@ public class Program
         // Toda função recursiva tem que ter o caso-Base-onde a função deve parar. e o caso- recursivo-onde a função chama ela mesma (cuidado com loop infinito)
         #region CAUCULAR FATORIAL
         Console.WriteLine("CAUCULAR FATORIAL");
-        var fatorial=Recursao.CaucularFat(5);
+        var fatorial = Recursao.CaucularFat(5);
         Console.WriteLine(fatorial);
         Console.WriteLine("COMPRIMENTAR");
         Recursao.Comprimentar("Marcus Vogado");
@@ -39,8 +39,8 @@ public class Program
 
         #region QUICKSORT
         Console.WriteLine("QUICKSORT");
-        var arr = new[] { 10, 5, 2, 3 };
-        Console.WriteLine(string.Join(", ",QuickSort.QuickSor(arr)));
+        var arr = new[] { 10, 5, 2, 3, 11, 15, 16, 17, 20, 26, 4, 12 };
+        Console.WriteLine(string.Join(", ", QuickSort.QuickSor(arr)));
         #endregion
         #region TABELAS HASH
         Console.WriteLine("TABELAS HASH");
